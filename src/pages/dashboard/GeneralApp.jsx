8ac37075ee75f47ec2c7284fbff52f0726894317
longhuux <huulong140103@gmail.com>
2024-04-01 22:28:23 +0700

    first commit

---
+++ src/pages/dashboard/GeneralApp.jsx
@@ -0,0 +1,14 @@
+import React, { Suspense, lazy } from "react";
+
+const Cat = lazy(() => import("../../components/Cat"));
+
+const GeneralApp = () => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default GeneralApp;
